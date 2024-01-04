cb9241b79be34f177756bd416e5495115512f8e6
404ptk <108901659+404ptk@users.noreply.github.com>
2024-01-04 05:11:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    var API_KEY = "AIzaSyCaHD8ew4SAagtZcgKmpcrc_AFvlP0zx18"
+
+    var video = ''
+
+    $("#form").submit(function(event){
+        event.preventDefault();
+
+        var search = $("#search").val()
+
+        $("#videos").empty();
+
+        videoSearch(API_KEY, search, 9)
+    })
+
+    function videoSearch(key, search, maxResults){
+        $.get("https://www.googleapis.com/youtube/v3/search?key=" + key
+        + "&type=video&part=snippet&maxResults=" + maxResults  
+        + "&q=" + search, function(data) {
+            console.log(data)
+
+            data.items.forEach(item => {
+                video = `
+
+                <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+
+                `
+
+                $("#videos").append(video)
+            });
+        })
+    }
+})
