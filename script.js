10b78ad31414767162a242f72cd17add4d3db74c
404ptk <108901659+404ptk@users.noreply.github.com>
2024-01-19 12:01:03 +0100

    video container change

--- script.js
+++ script.js
@@ -29,20 +29,62 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 var videoslist = data.items;
 
-                videoslist.forEach(function (item) {
+                videoslist.forEach(async function (item) {
+                    var title = item.snippet.title;
+                    var uploadDate = formatDate(item.snippet.publishedAt);
+
+                    // Pobierz liczbę wyświetleń za pomocą osobnego żądania
+                    var viewCount = await getViewCount(item.id.videoId);
+
+                    // Formatuj liczbę wyświetleń
+                    var formattedViewCount = formatViewCount(viewCount);
+
                     video = `
-                        <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+                        <div class="video-container">
+                            <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+                            <div class="video-details">
+                                <p><b>Tytuł:</b> ${title}</p>
+                                <p><b>Data dodania:</b> ${uploadDate}</p>
+                                <p><b>Wyświetlenia:</b> ${formattedViewCount}</p>
+                            </div>
+                        </div>
+                        <hr>
                     `;
 
                     document.getElementById("videos").innerHTML += video;
                 });
-
-                var zgloszenie = `<hr></hr>`;
-                document.getElementById("zgloszenie").innerHTML += zgloszenie;
             }
         };
 
         xhr.send();
+    }
+
+    async function getViewCount(videoId) {
+        // Pobierz liczbę wyświetleń za pomocą osobnego żądania
+        var statisticsResponse = await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=statistics&key=${API_KEY}`);
+        var statisticsData = await statisticsResponse.json();
+
+        // Sprawdź, czy statystyki są dostępne i pobierz liczbę wyświetleń
+        var viewCount = statisticsData.items.length > 0 ? statisticsData.items[0].statistics.viewCount : 'N/A';
+
+        return viewCount;
+    }
+
+    function formatDate(dateString) {
+        var options = { year: 'numeric', month: 'long', day: 'numeric' };
+        var formattedDate = new Date(dateString).toLocaleDateString('pl-PL', options);
+        return formattedDate;
+    }
+
+    // Funkcja do konwersji liczby wyświetleń na format "1mln" lub "563,5tys"
+    function formatViewCount(count) {
+        if (count >= 1000000) {
+            return (count / 1000000).toFixed(1) + 'mln';
+        } else if (count >= 1000) {
+            return (count / 1000).toFixed(1) + 'tys';
+        } else {
+            return count.toString();
+        }
     }
 });
 
@@ -59,10 +101,11 @@ function filtry() {
 
 var contact = document.getElementById("formularz");
 contact.style.display = "none";
-function contact_form(){
-    if(contact.style.display == "none"){
+
+function contact_form() {
+    if (contact.style.display == "none") {
         contact.style.display = "block";
-    }else{
+    } else {
         contact.style.display = "none";
     }
 }
